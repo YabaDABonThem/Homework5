2463ba86c3f2109fade55ab9f8a8cb009e165055
Yabadaba <allenbao01@gmail.com>
2021-11-23 22:19:12 -0800

    psuedocode and comments added

--- StockMarket.java
+++ StockMarket.java
@@ -1,4 +1,9 @@
+// Allen Bao
+// 11/23/21
+// A program that runs a stock market simulation by betting the amount the user inputs for 4 different traders.
+// Each trader has a different name and success rate, but starts off with the same amount of money (1000 dollars)
 
+// import scanner class
 import java.util.Scanner;
 
 // A "casino" class
@@ -6,54 +11,49 @@ public class StockMarket {
 
     public static void main(String Args[]) {
 
-        // four reference variables, trader1, trader2, trader3, trader4, 
-        // each of which references a new instance of an object of type Trader,
-        // created using the non-standard constructor
+        // create 4 instances of the trader class, each one representing a different trade
         Trader trader1 = new Trader("Trader1");
         Trader trader2 = new Trader("Trader2");
         Trader trader3 = new Trader("Trader3");
         Trader trader4 = new Trader("Trader4");
+
         // set stock change percentage (stockChangePercentage) for each player (sample percentages)
-        // trader1 has stock change percentage of 0.61
-        // trader2 has stock change percentage of 0.19
-        // trader3 has stock change percentage of 0.81
-        // trader4 has stock change percentage of 0.42
         trader1.setStockChangePercentage(0.61);
         trader2.setStockChangePercentage(0.19);
         trader3.setStockChangePercentage(0.81);
         trader4.setStockChangePercentage(0.42);        
-        // invoke the getTraderName and getStockChangePercentage methods
-        // for each Trader object, to retrieve that information, and print
-        // it to the screen
+
+        // print the name of each trader and their stockChangePercentage
         System.out.println("Name: " + trader1.getTraderName() + " \nStockChangePercentage: " + trader1.getStockChangePercentage());
         System.out.println("Name: " + trader2.getTraderName() + " \nStockChangePercentage: " + trader2.getStockChangePercentage());
         System.out.println("Name: " + trader3.getTraderName() + " \nStockChangePercentage: " + trader3.getStockChangePercentage());
         System.out.println("Name: " + trader4.getTraderName() + " \nStockChangePercentage: " + trader4.getStockChangePercentage());
-        // create a variable, keyboard, of type Scanner, and set it
-        // equal to a new object of type Scanner, that "listens" to
-        // System.in	
+
+        // create scanner
         Scanner keyboard = new Scanner(System.in);
-        // a while loop, with a "true" conditional
+        
+        // initialize variable used in loop
+        double dollarsSpend;
+        
+        // infinite while loop
         while (true) {
-            // For each of the four traders, retrieve the trader's name, how much money hey/she spent,
-            // how much money he/she currently has, and the trader's net profit. Print that information
-            // to the screen. Look at the homework handout for sample output					
-            // use a System.out.print statement to ask how much money each trader should bet. 
-            // Then declare a variable dollarsSpend of type double, and set its value to the keyboard's
-            // keystroke (be sure to use keyboard.nextDouble()).
-            // If the user enters 0, then issue a break statement, so that the while loop terminates
-            // Else, invoke the tradeSomeMore method of each Trader, and pass it the variable dollarsSpend	
+        
+            // print each trader's name, amount spent, the dollars made, and net profit
             System.out.println("\nName: " + trader1.getTraderName() + "\nDollars Spent: " + trader1.getDollarsSpent() + "\nBalance: " + trader1.getDollarsMade() + "\nNet Profit: " + trader1.getNetProfit());
             System.out.println("\nName: " + trader2.getTraderName() + "\nDollars Spent: " + trader2.getDollarsSpent() + "\nBalance: " + trader2.getDollarsMade() + "\nNet Profit: " + trader2.getNetProfit());
             System.out.println("\nName: " + trader3.getTraderName() + "\nDollars Spent: " + trader3.getDollarsSpent() + "\nBalance: " + trader3.getDollarsMade() + "\nNet Profit: " + trader3.getNetProfit());
             System.out.println("\nName: " + trader4.getTraderName() + "\nDollars Spent: " + trader4.getDollarsSpent() + "\nBalance: " + trader4.getDollarsMade() + "\nNet Profit: " + trader4.getNetProfit());
             
+            // ask the user how much they want to bet and store their input in dollarsSpend
             System.out.println("How much money do you want to bet? ");
-            double dollarsSpend = keyboard.nextDouble();
+            dollarsSpend = keyboard.nextDouble(); // Inputs that'd break the code would be strings and characters that aren't numbers
             
+            // if user bets zero dollars the while loop terminates
             if (dollarsSpend == 0) {
                break;
             }
+            
+            // bet the money the user inputted
             else {
                trader1.tradeSomeMore(dollarsSpend);
                trader2.tradeSomeMore(dollarsSpend);
--- Trader.java
+++ Trader.java
@@ -1,104 +1,75 @@
+// Allen Bao
+// 11/23/21
+// A trader class that trades money based off the amount given can give a few analytics about a specific trader.
+
 // Import the Random class
 import java.util.Random;
 
 // A Class that defines an object of type Trader
 public class Trader {
-    // fields; there are four of them, and each should be private:
-    // 
-    // traderName of type String
-    // stockChangePercentage of type double
-    // dollarsSpent of type double
-    // dollarsMade of type double
-    //
+
+    // Create fields that store trader properties
     private String traderName;
     private double stockChangePercentage;
     private double dollarsSpent;
     private double dollarsMade;
-    // method: getStockChangePercentage, public
-    // input: void
-    // output: double
-    // description: retrieves the value stockChangePercentage field
-    //
+    
+    // getter that retrieves the value stockChangePercentage field
     public double getStockChangePercentage() {
       return stockChangePercentage;
     }
-    // method: setStockChangePercentage , public
-    // input: percentage, which is of type double
-    // output: void
-    // description: assign the field stockChangePercentage to percentage
-    //
+    
+    // setter that assigns the field stockChangePercentage to percentage
     public void setStockChangePercentage(double percentage) {
       stockChangePercentage = percentage;
     }
-    // method: tradeSomeMore, public
-    // input parameter: dollars, which is of type double
-    // output: none 
-    // description (pseudocode):
-    //
-    // 1) increment field dollarsSpent by input value dollars
-    // 2) declare randomNumbers of be of type Random, and instantiate new random class
-    // 3) declare variable randomDouble of type double, and assign it the value output 
-    //    by the method nextDouble() of randomNumbers
-    // 4) if (randomDouble < stockChangePercentage){ 
-    //        println(traderName + " made");
-    //        dollarsMade += (dollars*2);
-    //    else {
-    //        println(traderName + " lost");
-    //        dollarsMade -= dollars*3;
-    //    }
-    //
-    public void tradeSomeMore(double dollars) {
     
+    // method that determines if a trader won or lost money, and how much they won/lost
+    public void tradeSomeMore(double dollars) {
+      // Assigns dollarsSpent to the amount of money the trader bets
       dollarsSpent += dollars;
       
+      // Creates a random double between zero and one
       Random randomNumbers = new Random();
       double randomDouble = randomNumbers.nextDouble();
       
+      // If the number generated is less that the trader's stockChangePercentage, the trader makes money
+      // Therefore, the greater the trader's stockChangePercentage, the more likely it is that they will make money
       if (randomDouble < stockChangePercentage) {
-         System.out.println(traderName + " made");
+         System.out.println(traderName + " made money");
+         // gain the dollars the trader bet
          dollarsMade += (dollars*2);
       }
       
+      // If the number is less than or equal to the trader's stockChangePercentage
       else {
-         System.out.println(traderName + " lost");
+         System.out.println(traderName + " lost money");
          dollarsMade -= dollars*3;
+         // lost the dollars the trader bet
       }
     }
-    // method: getTraderName, public
-    // input paramter: none
-    // output: value of field traderName
-    //
+    
+    // getter for the traderName field
     public String getTraderName() {
       return traderName;
     }
-    // method: getDollarsSpent, public
-    // input paramter: none
-    // output: value of field dollarsSpent
-    //
+    
+    // getter for the dollarsSpent field
     public double getDollarsSpent() {
       return dollarsSpent;
     }
-    // method: getDollarsMade, public
-    // input paramter: none
-    // output: value of field dollarsMade;
-    //
+    
+    // getter for the dollarsMade field
     public double getDollarsMade() {
       return dollarsMade;
     }
-    // method: getNetProfit, public
-    // input paramter: none
-    // output: result of (dollarsMade - dollarsSpent)
-    //
+    
+    // method that returns the trader's net profit
     public double getNetProfit() {
       return (dollarsMade - dollarsSpent);
     }
-    // Non-default constructor
-    // input paramter: name, which is of type String
-    // output: NA
-    //
-    // 1. set traderName to value in input argument name
-    // 2. set dollarsMade to 1000 (all Traders begin with $1000)
-    // 3. set dollarsSpent to 0 (all Traders begin neutrally)
+    
+    // constructor that sets the default values for each field
     public Trader(String name) {
         traderName = name;
         dollarsMade = 1000;
